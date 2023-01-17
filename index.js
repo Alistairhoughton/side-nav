adda1853835f4516c2b39f73c8287335df0475bf
Alistair Houghton <alistair.g.houghton@gmail.com>
2023-01-17 22:09:23 +0000

    updated files

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const links = document.querySelectorAll(".nav-link");
-
-
+const container = document.querySelector(".nav-link-holder");
 
 links.forEach((link) => {
   link.addEventListener("click", () => {
@@ -8,3 +7,29 @@ links.forEach((link) => {
     link.classList.add("active")
   });
 });
+
+
+
+// addMiddle = () => {
+//  links.forEach((el) => el.classList.toggle("middle")) 
+// }
+
+// container.addEventListener("mouseover", (addMiddle));
+// container.addEventListener("mouseout", (addMiddle));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
